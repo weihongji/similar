c4b61d5b586835662bc150e984164b4a850bb4a2
Jesse <weihongji@gmail.com>
2019-08-29 16:07:44 +0800

    first commit

---
+++ Similar/SimilarForm.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace Similar
+{
+	partial class SimilarForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimilarForm));
+			this.panelTop = new System.Windows.Forms.Panel();
+			this.labelDescription = new System.Windows.Forms.Label();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.statusLengthLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusLength = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusPositionLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusPosition = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusCharLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusChar = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusCodeLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusCode = new System.Windows.Forms.ToolStripStatusLabel();
+			this.panelButtons = new System.Windows.Forms.Panel();
+			this.chkCase = new System.Windows.Forms.CheckBox();
+			this.panelButtonsInner = new System.Windows.Forms.Panel();
+			this.btnRestore = new System.Windows.Forms.Button();
+			this.contextMenuRestore = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.menuClean = new System.Windows.Forms.ToolStripMenuItem();
+			this.btnCompress = new System.Windows.Forms.Button();
+			this.btnLocate = new System.Windows.Forms.Button();
+			this.panelText = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.btnUp = new System.Windows.Forms.Button();
+			this.txtUp = new System.Windows.Forms.TextBox();
+			this.btnDown = new System.Windows.Forms.Button();
+			this.txtDown = new System.Windows.Forms.TextBox();
+			this.panelTop.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			this.panelButtons.SuspendLayout();
+			this.panelButtonsInner.SuspendLayout();
+			this.contextMenuRestore.SuspendLayout();
+			this.panelText.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panelTop
+			// 
+			this.panelTop.Controls.Add(this.labelDescription);
+			this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panelTop.Location = new System.Drawing.Point(0, 0);
+			this.panelTop.Name = "panelTop";
+			this.panelTop.Size = new System.Drawing.Size(664, 32);
+			this.panelTop.TabIndex = 0;
+			// 
+			// labelDescription
+			// 
+			this.labelDescription.AutoSize = true;
+			this.labelDescription.Location = new System.Drawing.Point(3, 9);
+			this.labelDescription.Name = "labelDescription";
+			this.labelDescription.Size = new System.Drawing.Size(287, 12);
+			this.labelDescription.TabIndex = 0;
+			this.labelDescription.Text = "Compare two text blocks and show the difference";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLengthLabel,
+            this.statusLength,
+            this.statusPositionLabel,
+            this.statusPosition,
+            this.statusCharLabel,
+            this.statusChar,
+            this.statusCodeLabel,
+            this.statusCode});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 120);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(664, 26);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusBar";
+			// 
+			// statusLengthLabel
+			// 
+			this.statusLengthLabel.Name = "statusLengthLabel";
+			this.statusLengthLabel.Size = new System.Drawing.Size(50, 21);
+			this.statusLengthLabel.Text = "Length:";
+			// 
+			// statusLength
+			// 
+			this.statusLength.AutoSize = false;
+			this.statusLength.Name = "statusLength";
+			this.statusLength.Size = new System.Drawing.Size(180, 21);
+			this.statusLength.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// statusPositionLabel
+			// 
+			this.statusPositionLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+			this.statusPositionLabel.Name = "statusPositionLabel";
+			this.statusPositionLabel.Size = new System.Drawing.Size(80, 21);
+			this.statusPositionLabel.Text = "Position #1:";
+			// 
+			// statusPosition
+			// 
+			this.statusPosition.AutoSize = false;
+			this.statusPosition.Name = "statusPosition";
+			this.statusPosition.Size = new System.Drawing.Size(120, 21);
+			this.statusPosition.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// statusCharLabel
+			// 
+			this.statusCharLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+			this.statusCharLabel.Name = "statusCharLabel";
+			this.statusCharLabel.Size = new System.Drawing.Size(42, 21);
+			this.statusCharLabel.Text = "Char:";
+			// 
+			// statusChar
+			// 
+			this.statusChar.AutoSize = false;
+			this.statusChar.Name = "statusChar";
+			this.statusChar.Size = new System.Drawing.Size(60, 21);
+			this.statusChar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// statusCodeLabel
+			// 
+			this.statusCodeLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left;
+			this.statusCodeLabel.Name = "statusCodeLabel";
+			this.statusCodeLabel.Size = new System.Drawing.Size(46, 21);
+			this.statusCodeLabel.Text = "ASCII:";
+			// 
+			// statusCode
+			// 
+			this.statusCode.AutoSize = false;
+			this.statusCode.Name = "statusCode";
+			this.statusCode.Size = new System.Drawing.Size(60, 21);
+			this.statusCode.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// panelButtons
+			// 
+			this.panelButtons.Controls.Add(this.chkCase);
+			this.panelButtons.Controls.Add(this.panelButtonsInner);
+			this.panelButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelButtons.Location = new System.Drawing.Point(0, 90);
+			this.panelButtons.Name = "panelButtons";
+			this.panelButtons.Size = new System.Drawing.Size(664, 30);
+			this.panelButtons.TabIndex = 2;
+			// 
+			// chkCase
+			// 
+			this.chkCase.AutoSize = true;
+			this.chkCase.Checked = true;
+			this.chkCase.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.chkCase.Location = new System.Drawing.Point(71, 6);
+			this.chkCase.Name = "chkCase";
+			this.chkCase.Size = new System.Drawing.Size(108, 16);
+			this.chkCase.TabIndex = 0;
+			this.chkCase.TabStop = false;
+			this.chkCase.Text = "&Case sensitive";
+			this.chkCase.UseVisualStyleBackColor = true;
+			this.chkCase.Click += new System.EventHandler(this.chkCase_Click);
+			// 
+			// panelButtonsInner
+			// 
+			this.panelButtonsInner.Controls.Add(this.btnRestore);
+			this.panelButtonsInner.Controls.Add(this.btnCompress);
+			this.panelButtonsInner.Controls.Add(this.btnLocate);
+			this.panelButtonsInner.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panelButtonsInner.Location = new System.Drawing.Point(354, 0);
+			this.panelButtonsInner.Name = "panelButtonsInner";
+			this.panelButtonsInner.Size = new System.Drawing.Size(310, 30);
+			this.panelButtonsInner.TabIndex = 0;
+			// 
+			// btnRestore
+			// 
+			this.btnRestore.ContextMenuStrip = this.contextMenuRestore;
+			this.btnRestore.Location = new System.Drawing.Point(236, 2);
+			this.btnRestore.Name = "btnRestore";
+			this.btnRestore.Size = new System.Drawing.Size(64, 25);
+			this.btnRestore.TabIndex = 2;
+			this.btnRestore.TabStop = false;
+			this.btnRestore.Text = "&Restore";
+			this.btnRestore.UseVisualStyleBackColor = true;
+			this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+			// 
+			// contextMenuRestore
+			// 
+			this.contextMenuRestore.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuClean});
+			this.contextMenuRestore.Name = "contextMenuRestore";
+			this.contextMenuRestore.Size = new System.Drawing.Size(109, 26);
+			this.contextMenuRestore.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuRestore_Opening);
+			// 
+			// menuClean
+			// 
+			this.menuClean.Name = "menuClean";
+			this.menuClean.Size = new System.Drawing.Size(108, 22);
+			this.menuClean.Text = "&Clean";
+			this.menuClean.Click += new System.EventHandler(this.menuClean_Click);
+			// 
+			// btnCompress
+			// 
+			this.btnCompress.Location = new System.Drawing.Point(134, 2);
+			this.btnCompress.Name = "btnCompress";
+			this.btnCompress.Size = new System.Drawing.Size(64, 25);
+			this.btnCompress.TabIndex = 1;
+			this.btnCompress.TabStop = false;
+			this.btnCompress.Text = "Com&press";
+			this.btnCompress.UseVisualStyleBackColor = true;
+			this.btnCompress.Click += new System.EventHandler(this.btnCompress_Click);
+			// 
+			// btnLocate
+			// 
+			this.btnLocate.Location = new System.Drawing.Point(32, 2);
+			this.btnLocate.Name = "btnLocate";
+			this.btnLocate.Size = new System.Drawing.Size(64, 25);
+			this.btnLocate.TabIndex = 0;
+			this.btnLocate.TabStop = false;
+			this.btnLocate.Text = "&Locate";
+			this.btnLocate.UseVisualStyleBackColor = true;
+			this.btnLocate.Click += new System.EventHandler(this.btnLocate_Click);
+			// 
+			// panelText
+			// 
+			this.panelText.Controls.Add(this.tableLayoutPanel1);
+			this.panelText.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelText.Location = new System.Drawing.Point(0, 32);
+			this.panelText.Name = "panelText";
+			this.panelText.Size = new System.Drawing.Size(664, 58);
+			this.panelText.TabIndex = 1;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.btnUp, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtUp, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.btnDown, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.txtDown, 1, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(664, 58);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// btnUp
+			// 
+			this.btnUp.Location = new System.Drawing.Point(3, 3);
+			this.btnUp.Name = "btnUp";
+			this.btnUp.Size = new System.Drawing.Size(64, 23);
+			this.btnUp.TabIndex = 0;
+			this.btnUp.TabStop = false;
+			this.btnUp.Text = "Paste &1";
+			this.btnUp.UseVisualStyleBackColor = true;
+			this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+			// 
+			// txtUp
+			// 
+			this.txtUp.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtUp.Location = new System.Drawing.Point(73, 3);
+			this.txtUp.Multiline = true;
+			this.txtUp.Name = "txtUp";
+			this.txtUp.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtUp.Size = new System.Drawing.Size(588, 23);
+			this.txtUp.TabIndex = 1;
+			this.txtUp.Click += new System.EventHandler(this.TextBox_Click);
+			this.txtUp.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+			this.txtUp.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.txtUp.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+			this.txtUp.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyUp);
+			// 
+			// btnDown
+			// 
+			this.btnDown.Location = new System.Drawing.Point(3, 32);
+			this.btnDown.Name = "btnDown";
+			this.btnDown.Size = new System.Drawing.Size(64, 23);
+			this.btnDown.TabIndex = 2;
+			this.btnDown.TabStop = false;
+			this.btnDown.Text = "Paste &2";
+			this.btnDown.UseVisualStyleBackColor = true;
+			this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+			// 
+			// txtDown
+			// 
+			this.txtDown.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtDown.Location = new System.Drawing.Point(73, 32);
+			this.txtDown.Multiline = true;
+			this.txtDown.Name = "txtDown";
+			this.txtDown.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.txtDown.Size = new System.Drawing.Size(588, 23);
+			this.txtDown.TabIndex = 3;
+			this.txtDown.Click += new System.EventHandler(this.TextBox_Click);
+			this.txtDown.TextChanged += new System.EventHandler(this.TextBox_TextChanged);
+			this.txtDown.Enter += new System.EventHandler(this.TextBox_Enter);
+			this.txtDown.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyDown);
+			this.txtDown.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox_KeyUp);
+			// 
+			// SimilarForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(664, 146);
+			this.Controls.Add(this.panelText);
+			this.Controls.Add(this.panelButtons);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.panelTop);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.KeyPreview = true;
+			this.Name = "SimilarForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Similar";
+			this.TopMost = true;
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SimilarForm_FormClosing);
+			this.Load += new System.EventHandler(this.SimilarForm_Load);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SimilarForm_KeyDown);
+			this.Resize += new System.EventHandler(this.SimilarForm_Resize);
+			this.panelTop.ResumeLayout(false);
+			this.panelTop.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.panelButtons.ResumeLayout(false);
+			this.panelButtons.PerformLayout();
+			this.panelButtonsInner.ResumeLayout(false);
+			this.contextMenuRestore.ResumeLayout(false);
+			this.panelText.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panelTop;
+		private System.Windows.Forms.CheckBox chkCase;
+		private System.Windows.Forms.Label labelDescription;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.Panel panelButtons;
+		private System.Windows.Forms.Panel panelText;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Button btnUp;
+		private System.Windows.Forms.Panel panelButtonsInner;
+		private System.Windows.Forms.Button btnRestore;
+		private System.Windows.Forms.Button btnCompress;
+		private System.Windows.Forms.Button btnLocate;
+		private System.Windows.Forms.TextBox txtUp;
+		private System.Windows.Forms.Button btnDown;
+		private System.Windows.Forms.TextBox txtDown;
+		private System.Windows.Forms.ToolStripStatusLabel statusLengthLabel;
+		private System.Windows.Forms.ToolStripStatusLabel statusLength;
+		private System.Windows.Forms.ToolStripStatusLabel statusPositionLabel;
+		private System.Windows.Forms.ToolStripStatusLabel statusPosition;
+		private System.Windows.Forms.ToolStripStatusLabel statusCharLabel;
+		private System.Windows.Forms.ToolStripStatusLabel statusChar;
+		private System.Windows.Forms.ToolStripStatusLabel statusCodeLabel;
+		private System.Windows.Forms.ToolStripStatusLabel statusCode;
+		private System.Windows.Forms.ContextMenuStrip contextMenuRestore;
+		private System.Windows.Forms.ToolStripMenuItem menuClean;
+	}
+}
+
---
+++ Similar/SimilarForm.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Similar
+{
+	public partial class SimilarForm : Form
+	{
+		private readonly string BUTTON_COMPRESS = "Com&press";
+		private readonly string BUTTON_EXPAND = "Ex&pand";
+
+		private readonly int KEYVALUE_PLUS = 187;
+		private readonly int KEYVALUE_MINUS = 189;
+
+		private readonly int FORM_HEIGHT_MIN = 185;
+		private readonly int AUTO_EXPAND_MAX = 9;
+
+		private string text1 = "";
+		private string text2 = "";
+
+		private bool clipboardCleaned = false;
+		private TextBox tbox2Locate = null;
+
+		public SimilarForm() {
+			InitializeComponent();
+		}
+
+		#region "Form Events"
+		private void SimilarForm_Load(object sender, EventArgs e) {
+			ShowLengthStatus();
+			ShowPositionStatus();
+
+			tbox2Locate = txtUp;
+		}
+
+		private void SimilarForm_FormClosing(object sender, FormClosingEventArgs e) {
+			if (this.clipboardCleaned && string.IsNullOrEmpty(Clipboard.GetText())) {
+				var result = MessageBox.Show("Windows clipboard has been cleaned. Continue exiting the program?", this.Text, MessageBoxButtons.OKCancel);
+				if (result == DialogResult.Cancel) {
+					e.Cancel = true;
+				}
+			}
+		}
+
+		private void SimilarForm_Resize(object sender, EventArgs e) {
+			if (this.TopMost && this.WindowState == FormWindowState.Maximized) { // Don't always be on top when maximized.
+				this.TopMost = false;
+			}
+			else if (!this.TopMost && this.WindowState == FormWindowState.Normal) {
+				this.TopMost = true;
+			}
+		}
+
+		private void SimilarForm_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.Escape) { // ESC
+				this.WindowState = FormWindowState.Minimized;
+			}
+			else if (e.KeyCode == Keys.X && e.Alt) { // ALT + "X"
+				e.Handled = e.SuppressKeyPress = true;
+				Application.Exit();
+			}
+			else if (e.Control && e.KeyValue == KEYVALUE_MINUS) { // CTRL + "-"
+				ResizeForm(0, -60);
+			}
+			else if (e.Control && e.KeyValue == KEYVALUE_PLUS) { // CTRL + "+"
+				ResizeForm(0, 60);
+			}
+			else if (e.Control && e.KeyValue == '0') { // CTRL + 1
+				ResizeForm(FORM_HEIGHT_MIN);
+			}
+			else if (e.Control && e.KeyValue == '9') { // CTRL + 9
+				this.WindowState = this.WindowState == FormWindowState.Normal ? FormWindowState.Maximized : FormWindowState.Normal;
+			}
+		}
+
+		#endregion
+
+		#region "Text Boxes"
+		private void btnUp_Click(object sender, EventArgs e) {
+			PasteText(txtUp);
+			IncreaseHeight();
+		}
+
+		private void btnDown_Click(object sender, EventArgs e) {
+			PasteText(txtDown);
+			IncreaseHeight();
+		}
+
+		private void TextBox_KeyDown(object sender, KeyEventArgs e) {
+			if (e.KeyCode == Keys.A && e.Control) {
+				SelectAllText((TextBox)sender);
+				e.Handled = e.SuppressKeyPress = true;
+			}
+		}
+
+		private void TextBox_KeyUp(object sender, KeyEventArgs e) {
+			ShowPositionStatus();
+		}
+
+		private void TextBox_TextChanged(object sender, EventArgs e) {
+			ShowLengthStatus();
+			ShowPositionStatus();
+		}
+
+		private void TextBox_Click(object sender, EventArgs e) {
+			this.tbox2Locate = (TextBox)sender;
+			ShowPositionStatus();
+		}
+
+		private void TextBox_Enter(object sender, EventArgs e) {
+			this.tbox2Locate = (TextBox)sender;
+		}
+		#endregion
+
+		#region "Location Buttons"
+		private void btnLocate_Click(object sender, EventArgs e) {
+			// Just in case. This button should be disabled when the two texts are same.
+			if (txtUp.Text.Equals(txtDown.Text)) {
+				return;
+			}
+
+			// Save existing text for later restore
+			if (String.IsNullOrEmpty(text1) && txtUp.Text.Length > 0) {
+				text1 = txtUp.Text;
+			}
+			if (String.IsNullOrEmpty(text2) && txtDown.Text.Length > 0) {
+				text2 = txtDown.Text;
+			}
+
+			// Get position of the first difference
+			char[] s1 = txtUp.Text.ToCharArray();
+			char[] s2 = txtDown.Text.ToCharArray();
+			int len1 = s1.Length;
+			int len2 = s2.Length;
+			int lenMin = Math.Min(len1, len2);
+			int index = -1;
+			for (int i = 0; i < lenMin; i++) {
+				if (s1[i] != s2[i]) {
+					index = i;
+					break;
+				}
+			}
+			if (index < 0 && len1 != len2) {
+				index = lenMin;
+			}
+
+			// Show the different character
+			if (index < 0) { // Should never happen
+				return;
+			}
+			var tbox = this.tbox2Locate;
+			SelectDifferentChar(tbox, index);
+			ShowPositionStatus();
+			this.tbox2Locate = this.tbox2Locate == txtUp ? txtDown : txtUp;
+		}
+
+		private void btnCompress_Click(object sender, EventArgs e) {
+			if (btnCompress.Text.Equals(BUTTON_COMPRESS)) {
+				// Save existing text for later restore
+				if (String.IsNullOrEmpty(text1) && txtUp.Text.Length > 0) {
+					text1 = txtUp.Text;
+				}
+				if (String.IsNullOrEmpty(text2) && txtDown.Text.Length > 0) {
+					text2 = txtDown.Text;
+				}
+
+				txtUp.Text = CompressText(txtUp.Text);
+				txtDown.Text = CompressText(txtDown.Text);
+
+				btnCompress.Text = BUTTON_EXPAND;
+			}
+			else { // Expand
+				txtUp.Text = ExpandText(txtUp.Text);
+				txtDown.Text = ExpandText(txtDown.Text);
+
+				this.btnCompress.Text = BUTTON_COMPRESS;
+			}
+		}
+
+		private void btnRestore_Click(object sender, EventArgs e) {
+			var restored = false;
+			if (text1.Length > 0) {
+				txtUp.Text = text1;
+				restored = true;
+			}
+			if (text2.Length > 0) {
+				txtDown.Text = text2;
+				restored = true;
+			}
+			if (restored) {
+				this.btnCompress.Text = BUTTON_COMPRESS;
+			}
+		}
+
+		private void contextMenuRestore_Opening(object sender, CancelEventArgs e) {
+			if (String.IsNullOrEmpty(text1 + text2) && menuClean.Enabled) {
+				menuClean.Enabled = false;
+			}
+			else if ((text1 + text2).Length > 0 && !menuClean.Enabled) {
+				menuClean.Enabled = true;
+			}
+		}
+
+		private void menuClean_Click(object sender, EventArgs e) {
+			text1 = text2 = "";
+		}
+
+		private void chkCase_Click(object sender, EventArgs e) {
+			ShowLengthStatus();
+		}
+		#endregion
+
+		private void ResizeForm(int height, int increase = 0) {
+			if (this.WindowState != FormWindowState.Normal) {
+				return;
+			}
+
+			int newHeight = 0;
+			if (height > 0) {
+				newHeight = height;
+			}
+			else {
+				newHeight = this.Height + increase;
+			}
+
+			if (newHeight <= FORM_HEIGHT_MIN) {
+				newHeight = FORM_HEIGHT_MIN;
+			}
+			if (this.Height != newHeight) {
+				this.Height = newHeight;
+			}
+		}
+
+		private void PasteText(TextBox textbox) {
+			textbox.Text = Clipboard.GetText();
+			Clipboard.Clear();
+			if (textbox.Text.Length > 0) {
+				this.clipboardCleaned = true;
+			}
+		}
+
+		private void IncreaseHeight() {
+			int lines = txtUp.Text.Count<char>(x => x == '\n') + 1;
+			int requiredLines = Math.Min(lines, AUTO_EXPAND_MAX);
+			int requiredHeight = FORM_HEIGHT_MIN + (requiredLines - 1) * 30;
+			if (this.Height < requiredHeight) {
+				ResizeForm(requiredHeight);
+			}
+		}
+
+		private void SelectAllText(TextBox tbox) {
+			tbox.SelectAll();
+		}
+
+		private TextBox GetActiveTextBox(bool forceOne = true) {
+			TextBox tbox = null;
+			if (this.ActiveControl != null && this.ActiveControl.GetType() == typeof(TextBox)) {
+				tbox = (TextBox)this.ActiveControl;
+			}
+			if (tbox == null && forceOne) {
+				tbox = txtUp;
+			}
+			return tbox;
+		}
+
+		private string CompressText(string s) {
+			if (String.IsNullOrWhiteSpace(s)) {
+				return string.Empty;
+			}
+			s = s.Replace('\n', '|');
+			s = Regex.Replace(s, @"\s*\|\s*", "|"); // Removing blank characters like space, tab or \r.
+			s = Regex.Replace(s, @"\|{2,}", "|"); // Shrink multiple "|" to one.
+			s = Regex.Replace(s, @"\s+", " "); // Shrink multiple blank characters to one space (32).
+			return s;
+		}
+
+		private string ExpandText(string s) {
+			if (String.IsNullOrWhiteSpace(s)) {
+				return string.Empty;
+			}
+			s = s.Replace("|", "\r\n"); // Replace all vertical bars to new lines. No matter they are generated by compressing or exist in original text.
+			return s;
+		}
+
+		private void ShowLengthStatus() {
+			var s1 = txtUp.Text;
+			var s2 = txtDown.Text;
+			var same = s1.Equals(s2, chkCase.Checked ? StringComparison.CurrentCulture : StringComparison.CurrentCultureIgnoreCase);
+
+			// Status bar
+			var lengthText = "";
+			if (same) {
+				lengthText = String.Format("{0} - Identical", FormatNumber(s1.Length));
+			}
+			else if (s1.Length == s2.Length) {
+				lengthText = String.Format("{0} - Different", FormatNumber(s1.Length));
+			}
+			else {
+				lengthText = String.Format("{0} {2} {1}", FormatNumber(s1.Length), FormatNumber(s2.Length), s1.Length > s2.Length ? ">" : "<");
+			}
+			statusLength.Text = lengthText;
+
+			// Locate button
+			if (same && btnLocate.Enabled) {
+				btnLocate.Enabled = false;
+			}
+			else if (!same && !btnLocate.Enabled) {
+				btnLocate.Enabled = true;
+			}
+		}
+
+		private void ShowPositionStatus() {
+			var tbox = GetActiveTextBox();
+			var index = tbox.SelectionStart;
+			var length = tbox.Text.Length;
+			if (length > 0) {
+				if (tbox.SelectionLength > 0) {
+					index++;
+				}
+				statusPosition.Text = String.Format("{0} / {1}", FormatNumber(index), tbox.Text.Length);
+			}
+			else {
+				statusPosition.Text = "0";
+			}
+			statusPositionLabel.Text = String.Format("Position #{0}: ", tbox.Name.IndexOf("Up") > 0 ? "1" : "2");
+		}
+
+		private void ShowCharStatus(string c) {
+			string charText = "";
+			switch (c) {
+				case "":
+					charText = "to end";
+					break;
+				case "\t":
+					charText = "Tab";
+					break;
+				case "\r":
+					charText = @"\r";
+					break;
+				case "\n":
+					charText = @"\n";
+					break;
+				case " ":
+					charText = "Space";
+					break;
+				default:
+					charText = c;
+					break;
+			}
+			statusChar.Text = charText;
+			int code = String.IsNullOrEmpty(c) ? 0 : (int)c[0];
+			string hex = code.ToString("X");
+			statusCode.Text = String.Format("{0} (x{1})", code, hex.PadLeft(2, '0'));
+		}
+
+		private void SelectDifferentChar(TextBox tbox, int index) {
+			tbox.Focus();
+			if (index < tbox.Text.Length) {
+				tbox.Select(index, 1);
+			}
+			else {
+				tbox.Select(index, 0);
+			}
+			ShowCharStatus(tbox.SelectedText);
+		}
+
+		private string FormatNumber(int n) {
+			return n.ToString("#,0");
+		}
+	}
+}
